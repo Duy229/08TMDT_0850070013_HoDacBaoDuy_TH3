18512fc3655d2f2fa6d26ff311a80c7701787269
Duy229 <hodacbaoduy229@gmail.com>
2023-08-04 07:03:06 +0700

    th3

---
+++ e_commerce_app_ui/lib/screens/profile/profile_screen.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:e_commerce_app_ui/components/coustom_bottom_nav_bar.dart';
+import 'package:e_commerce_app_ui/enums.dart';
+
+import 'components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+  static String routeName = "/profile";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: Body(),
+      bottomNavigationBar: const CustomBottomNavBar(selectedMenu: MenuState.profile),
+    );
+  }
+}
